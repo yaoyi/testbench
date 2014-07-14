edca94f202fc5093a27934600316be559033dab9
ben <ben@bmcd.co>
2014-07-14 14:22:34 +0800

    save

--- myapp.rb
+++ myapp.rb
@@ -4,6 +4,21 @@ require 'json'
 require 'dalli'
 set :cache, Dalli::Client.new
 
+# initialize log
+require 'logger'
+Dir.mkdir('log') unless File.exist?('log')
+class ::Logger; alias_method :write, :<<; end
+case ENV["RACK_ENV"]
+when "production"
+  logger = ::Logger.new("log/production.log")
+  logger.level = ::Logger::WARN
+when "development"
+  logger = ::Logger.new(STDOUT)
+  logger.level = ::Logger::DEBUG
+else
+  logger = ::Logger.new("/dev/null")
+end
+
 Mongoid.load!("config/mongoid.yml")
 class Qrcode
     include Mongoid::Document
