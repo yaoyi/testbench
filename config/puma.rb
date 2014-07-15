7f01569093ab0f9d7513e08b855483525f1b853a
ben <ben@bmcd.co>
2014-07-15 15:53:39 +0800

    save

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,12 @@
-threads 8,32
-workers 3 
-preload_app!
+# threads 8,32
+# workers 3 
+# preload_app!
 
-bind  "unix:///u/apps/intercity_sample_app/current/tmp/puma/appname-puma.sock"
-pidfile "/u/apps/intercity_sample_app/current/tmp/puma/pid"
-state_path "/u/apps/intercity_sample_app/current/tmp/puma/state"
+# bind  "unix:///u/apps/intercity_sample_app/current/tmp/puma/appname-puma.sock"
+# pidfile "/u/apps/intercity_sample_app/current/tmp/puma/pid"
+# state_path "/u/apps/intercity_sample_app/current/tmp/puma/state"
 
-activate_control_app
+# activate_control_app
 
-rackup      DefaultRackup
-environment 'production'
+# rackup      DefaultRackup
+# environment 'production'
