1ad73c27db579c990d8d5a14c0ec53c2159e52fc
ben <ben@bmcd.co>
2014-07-15 15:43:53 +0800

    save

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,8 +51,8 @@ end
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state, "#{current_path}/tmp/pids/puma.state"
 set :puma_pid, "#{current_path}/tmp/pids/puma.pid"
-set :puma_bind, "unix://#{current_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-set :puma_conf, "#{current_path}/config/puma.rb"
+set :puma_bind, %w(unix://#{current_path}/tmp/sockets/puma0.sock unix://#{current_path}/tmp/sockets/puma1.sock unix://#{current_path}/tmp/sockets/puma2.sock)    #accept array for multi-bind
+# set :puma_conf, "#{current_path}/config/puma.rb"
 set :puma_access_log, "#{current_path}/log/puma_error.log"
 set :puma_error_log, "#{current_path}/log/puma_access.log"
 set :puma_role, :app
