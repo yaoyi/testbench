25178db125e65d5c6b471339b98be0d827f944ca
ben <ben@bmcd.co>
2014-07-17 16:55:56 +0800

    save

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -19,3 +19,42 @@ pid "#{@dir}tmp/pids/unicorn.pid"
 stderr_path "#{@dir}log/unicorn.stderr.log"
 stdout_path "#{@dir}log/unicorn.stdout.log"
 
+preload_app true
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = "/u/apps/intercity_sample_app/current/Gemfile"
+end
+
+before_fork do |server, worker|
+  # Disconnect since the database connection will not carry over
+  if defined? ActiveRecord::Base
+    ActiveRecord::Base.connection.disconnect!
+  end
+
+  # Quit the old rainbows process
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+
+  if defined?(Resque)
+    Resque.redis.quit
+  end
+
+  sleep 1
+end
+
+after_fork do |server, worker|
+  # Start up the database connection again in the worker
+  if defined?(ActiveRecord::Base)
+    ActiveRecord::Base.establish_connection
+  end
+
+  if defined?(Resque)
+    Resque.redis = 'localhost:6379'
+  end
+end
