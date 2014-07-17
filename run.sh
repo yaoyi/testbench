9ed3ab81f1a28aaa7afd7a552a768f6e960cb239
ben <ben@bmcd.co>
2014-07-17 18:08:56 +0800

    save

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-bundle exec unicorn -p 9000
-#bundle exec unicorn -p 9000 -c config/unicorn.rb
+#bundle exec unicorn -p 9000
+bundle exec unicorn -p 9000 -c config/unicorn-development.rb
