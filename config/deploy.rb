e4809f183bb55ce1646ca5167776fb7d515d23b7
ben <ben@bmcd.co>
2014-07-28 12:51:24 +0800

    update deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :ssh_options, {
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/thin/production.yml config/thin/staging.yml}
+# set :linked_files, %w{config/thin/production.yml config/thin/staging.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -16,6 +16,8 @@ role :db,  %w{deploy@thin-sinatra-mongo.testbench.staging-1.foodspy.io}
 # extended properties on the server.
 server 'thin-sinatra-mongo.testbench.staging-1.foodspy.io', user: 'deploy', roles: %w{web app db}, my_property: :my_value
 
+set :stage, 'staging'
+
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
 # you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
