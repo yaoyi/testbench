0822db47e5d2e11384641c4e2321739ba28e2c0b
ben <ben@bmcd.co>
2014-07-28 11:22:13 +0800

    update deploy conf

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :ssh_options, {
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+# set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
