3fc3b4f356c965d6012bb6f55d1ee6df052de084
ben <ben@bmcd.co>
2014-07-28 11:04:01 +0800

    update deploy conf

--- myapp.rb
+++ myapp.rb
@@ -2,20 +2,6 @@ require 'sinatra'
 require 'mongoid'
 require 'json'
 configure { set :server, :puma }
-# initialize log
-require 'logger'
-Dir.mkdir('log') unless File.exist?('log')
-class ::Logger; alias_method :write, :<<; end
-case ENV["RACK_ENV"]
-when "production"
-  logger = ::Logger.new("log/production.log")
-  logger.level = ::Logger::WARN
-when "development"
-  logger = ::Logger.new(STDOUT)
-  logger.level = ::Logger::DEBUG
-else
-  logger = ::Logger.new("/dev/null")
-end
 
 Mongoid.load!("config/mongoid.yml")
 class Qrcode
@@ -29,9 +15,4 @@ end
 get '/:id' do
 	@qrcode = Qrcode.find params[:id]
 	@qrcode.to_json
-end
-
-get '/qrcodes.json' do
-	@qrcodes = Qrcode.all
-	@qrcodes.to_json
 end
