12528dc42c0bb1578e949c6d6aa5c88588613869
ben <ben@bmcd.co>
2014-07-28 14:42:32 +0800

    update deploy conf

--- myapp.rb
+++ myapp.rb
@@ -4,21 +4,6 @@ require 'unicorn'
 require 'mongoid'
 require 'json'
 
-# initialize log
-require 'logger'
-Dir.mkdir('log') unless File.exist?('log')
-class ::Logger; alias_method :write, :<<; end
-case ENV["RACK_ENV"]
-when "production"
-  logger = ::Logger.new("log/production.log")
-  logger.level = ::Logger::WARN
-when "development"
-  logger = ::Logger.new(STDOUT)
-  logger.level = ::Logger::DEBUG
-else
-  logger = ::Logger.new("/dev/null")
-end
-
 Mongoid.load!("config/mongoid.yml")
 class Qrcode
     include Mongoid::Document
@@ -31,9 +16,4 @@ end
 get '/:id' do
 	@qrcode = Qrcode.find params[:id]
 	@qrcode.to_json
-end
-
-get '/qrcodes.json' do
-	@qrcodes = Qrcode.all
-	@qrcodes.to_json
 end
