1d61b6c8dea204caa674e758a5b4dec675c35f44
ben <ben@bmcd.co>
2014-07-28 11:35:24 +0800

    update deploy conf

--- myapp.rb
+++ myapp.rb
@@ -2,21 +2,6 @@ require 'sinatra'
 require 'mongoid'
 require 'json'
 
-# initialize log
-require 'logger'
-Dir.mkdir('log') unless File.exist?('log')
-class ::Logger; alias_method :write, :<<; end
-case ENV["RACK_ENV"]
-when "production"
-  logger = ::Logger.new("log/production.log")
-  logger.level = ::Logger::WARN
-when "development"
-  logger = ::Logger.new(STDOUT)
-  logger.level = ::Logger::DEBUG
-else
-  logger = ::Logger.new("/dev/null")
-end
-
 Mongoid.load!("config/mongoid.yml")
 class Qrcode
     include Mongoid::Document
